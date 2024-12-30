88deb5e7e448fe3c31a24c41ea9f06cffabc7779
ashar708 <aarya.sharma.development@gmail.com>
2024-12-30 22:50:01 +0530

    Update OrderController.js

--- backend/Controllers/OrderController.js
+++ backend/Controllers/OrderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //placing using order from front-end
 const placeOrder = async (req,res) => {
-    const frontend_url = "http://localhost:5174";
+    const frontend_url = "https://swiggy-clone-frontend-2ft7.onrender.com/";
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
